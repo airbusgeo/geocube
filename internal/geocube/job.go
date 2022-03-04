830e57b05435a14d980e022d95ef8b51958ebc7a
Vincent Varoquaux <vincent.varoquaux@airbus.com>
2022-03-04 09:14:20 +0100

    job: Forced Cancelation of DeletionInProgress

--- internal/geocube/job.go
+++ internal/geocube/job.go
@@ -552,6 +552,11 @@ func (j *Job) triggerDeletion(evt JobEvent) bool {
 		}
 	case JobStateDELETIONINPROGRESS:
 		switch evt.Status {
+		case CancelledByUserForced:
+			if !j.Waiting {
+				j.LogMsg(WARN, "Deletion has been cancelled, but files may have already been deleted")
+			}
+			return j.changeState(JobStateABORTED)
 		case CancelledByUser:
 			if j.Waiting {
 				return j.changeState(JobStateABORTED)
